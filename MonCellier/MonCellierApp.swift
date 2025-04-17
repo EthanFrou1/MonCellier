dcb888a6a6af593cb71a4b991d1822a182c2c1d2
EthanFrou1 <ethanfrou1@gmail.com>
2025-04-17 13:58:38 +0200

    Initial Commit

---
+++ MonCellier/MonCellierApp.swift
@@ -0,0 +1,17 @@
+//
+//  MonCellierApp.swift
+//  MonCellier
+//
+//  Created by Stagiaire on 17/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct MonCellierApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
